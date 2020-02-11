d4818e0fb005f6cdfb225b93a4545dee4ccb946b
ashutosh <sonutiwari2329@gmail.com>
2020-02-11 18:34:58 +0530

    Add files via upload

---
+++ email.py
@@ -0,0 +1,8 @@
+print('Program to create email adress ')
+first_name=input("enter your first name:")
+last_name=input("enter your last name: ")
+any_digit=input("enter any character to be inserted:")
+email_address=first_name+last_name+any_digit+"@gmail.com"
+print(email_address)
+
+
