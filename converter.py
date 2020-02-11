810784b6de1393c41dd8f53c05aff9973cca0a12
ashutosh <sonutiwari2329@gmail.com>
2020-02-11 18:53:56 +0530

    Add files via upload

---
+++ converter.py
@@ -0,0 +1,7 @@
+print("code to convert Km to miles")
+in_km=float(input("enter the distance in km:"))
+mile=1.609344
+in_miles=round((in_km/mile),5)
+print(f"distance in miles is:",in_miles,"Miles")
+                  
+                  
