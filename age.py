2d209ddf77728679e18f0c89d86efa9d2cdf195a
ashutosh <sonutiwari2329@gmail.com>
2020-02-14 19:25:11 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,12 @@
+my_age=20
+user_age=int(input("enter your age:"))
+if my_age>user_age:
+    print("you are younger than u")
+elif my_age<user_age:
+    print("you are elder than you")
+else:
+    print("we are of the same age")
+    
+        
+
+                
